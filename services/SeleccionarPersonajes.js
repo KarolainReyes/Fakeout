1229dd83a97f5462faabe77058d52f6f69a4a20a
Andre07G <eleal087@gmail.com>
2025-09-18 01:40:31 -0500

    feat: :sparkles: Creacion de clases para seleccionar y visualizar los personajes dentro del json de personajes
    by@Andres07g

---
+++ services/SeleccionarPersonajes.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import inquirer from "inquirer";
+
+export class SeleccionPersonajes {
+  static async mostrarPersonajes(rutaArchivo = "../data/personajes.json") {
+    const personajes = JSON.parse(fs.readFileSync(rutaArchivo, "utf-8"));
+
+    const { seleccion } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "seleccion",
+        message: "? Selecciona un personaje:",
+        choices: personajes.map((p, index) => ({
+          name: `${index} - ${p.nombre}`,
+          value: index,
+        })),
+      },
+    ]);
+    console.log("Personaje seleccionado correctamente");
+    return personajes[seleccion];
+  }
+}
+
